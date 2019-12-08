26e74d177fa3c9a30bf4fd0efe3de833eb799284
Chroberry <chroberry@gmail.com>
2019-12-07 17:35:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./components/FirstComponent";
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Babby's First React App</h1>
+        <p>Stuff</p>
+      </div>
+      <FirstComponent />
     </div>
   );
 }
---
+++ src/components/FirstComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./FirstComponent.css";
+
+// testing component import
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent P-Spin">
+      <div>
+        <p id="spinny">This is the first test component.</p>
+      </div>
+    </div>
+  );
+}
+
+export default FirstComponent;
